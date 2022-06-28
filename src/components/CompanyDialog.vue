eb4d19417af7ed1ce48c1a517e3c851c3ca4bd57
unknown <ojedokunp977@gmail.com>
2022-06-28 15:30:13 +0100

    form dialog component done

---
+++ src/components/CompanyDialog.vue
@@ -0,0 +1,94 @@
+<template>
+  <el-dialog title="Sign up to Job Forage Agencies">
+    <form class="w-full rounded bg-slate-100 mx-auto p-3 md:w-10/12 md:p-5">
+      <h5 class="font-bold mb-3">Register</h5>
+      <div class="mb-3">
+        <input
+          type="text"
+          id="company_name"
+          class="border border-slate-200 text-xs rounded block w-full p-2.5"
+          placeholder="Company Name"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="email"
+          id="company_email"
+          class="border border-slate-200 text-xs rounded block w-full p-2.5"
+          placeholder="Company Email Address"
+          required
+        />
+      </div>
+      <div class="grid gap-3 mb-3 lg:grid-cols-2">
+        <div>
+          <input
+            type="text"
+            id="first_name"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="First Name"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            id="last_name"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Last Name"
+            required
+          />
+        </div>
+      </div>
+      <div class="mb-3">
+        <input
+          type="password"
+          id="password"
+          class="border border-slate-200 text-xs rounded block w-full p-2.5"
+          placeholder="Password (Minimum of 8 characters)"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="password"
+          id="confirm_password"
+          class="border border-slate-200 text-xs rounded block w-full p-2.5"
+          placeholder="Repeat password"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="tel"
+          id="phone"
+          class="border border-slate-200 text-xs rounded block w-full p-2.5"
+          placeholder="Phone number"
+          pattern="[0-9]{4}-[0-9]{3}-[0-9]{4}"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          id="location"
+          class="border border-slate-200 text-xs rounded block w-full p-2.5"
+          placeholder="Company Address"
+          required
+        />
+      </div>
+      <div class="sm:flex">
+        <button
+          type="submit"
+          class="block bg-white border-2 border-slate-200 cursor-pointer font-bold mb-3 py-2.5 px-4 text-xs sm:mr-3 sm:mb-0"
+        >
+          Upload Your Company Logo
+        </button>
+      </div>
+    </form>
+    <p class="mt-1 text-center text-xs">
+      By registering with Job Forage you agree to our Privacy Policy and Terms &
+      Conditions
+    </p>
+  </el-dialog>
+</template>
--- src/views/RecruitView.vue
+++ src/views/RecruitView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { ref } from "vue";
+
+const companyDialog = ref(false);
+</script>
+
 <template>
   <main
     class="px-4 py-4 sm:px-6 md:pb-4 md:pt-6 lg:pb-5 lg:pt-7 lg:px-32 xl:px-48 xl:pb-7 xl:pt-9"
@@ -20,6 +26,7 @@
           </p>
           <button
             class="block bg-white border-2 border-slate-200 cursor-pointer font-semibold py-2.5 px-4 text-xs"
+            @click="companyDialog = true"
           >
             Advertise Now
           </button>
@@ -103,5 +110,98 @@
         />
       </div>
     </div>
+
+    <el-dialog v-model="companyDialog" title="Sign up to Job Forage Agencies">
+      <form class="w-full rounded bg-slate-100 mx-auto p-3 md:w-10/12 md:p-5">
+        <h5 class="font-bold mb-3">Register</h5>
+        <div class="mb-3">
+          <input
+            type="text"
+            id="company_name"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Company Name"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="email"
+            id="company_email"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Company Email Address"
+            required
+          />
+        </div>
+        <div class="grid gap-3 mb-3 lg:grid-cols-2">
+          <div>
+            <input
+              type="text"
+              id="first_name"
+              class="border border-slate-200 text-xs rounded block w-full p-2.5"
+              placeholder="First Name"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              id="last_name"
+              class="border border-slate-200 text-xs rounded block w-full p-2.5"
+              placeholder="Last Name"
+              required
+            />
+          </div>
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            id="password"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Password (Minimum of 8 characters)"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            id="confirm_password"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Repeat password"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="tel"
+            id="phone"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Phone number"
+            pattern="[0-9]{4}-[0-9]{3}-[0-9]{4}"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="text"
+            id="location"
+            class="border border-slate-200 text-xs rounded block w-full p-2.5"
+            placeholder="Company Address"
+            required
+          />
+        </div>
+        <div class="sm:flex">
+          <button
+            type="submit"
+            class="block bg-white border-2 border-slate-200 cursor-pointer font-bold mb-3 py-2.5 px-4 text-xs sm:mr-3 sm:mb-0"
+          >
+            Upload Your Company Logo
+          </button>
+        </div>
+      </form>
+      <p class="mt-1 text-center text-xs">
+        By registering with Job Forage you agree to our Privacy Policy and Terms
+        & Conditions
+      </p>
+    </el-dialog>
   </main>
 </template>
